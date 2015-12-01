e76ece258dc2c9a6e1d38e22f8774b4688515421
Zhao Yang <yz931129@gmail.com>
2015-11-30 18:48:40 -0800

    commented and cleaned up Zhao's Matlab script

--- CS 145/main_zhao.m
+++ CS 145/main_zhao.m
@@ -1,8 +1,10 @@
+% Parse files. Not necessary if files already exist.
 train_file = '.\train_2000.csv';
 test_file = '.\test_2000.csv';
 systemCommand = 'C:\Python27\python27.exe generate_train_test_data_1.py 2000';
 system(systemCommand);
 
+% Generate training features
 fid=fopen(train_file);
 tline = fgetl(fid);
 num_col = length(find(tline==',')) + 1;
@@ -10,6 +12,7 @@ format = ['%d %s' repmat('%f', 1, num_col - 12)];
 c = textscan(fid, format, 'Delimiter', ',');
 fclose(fid);
 
+% Convert labels
 train_features = c(:, 3:end);
 train_features = cell2mat(train_features);
 
@@ -21,8 +24,8 @@ train_labels = zeros(length(c{2}), 1);
 for i = 1 : length(c{2})
     train_labels(i) = label_dict(c{2}{i});
 end
-%train_features = train_features(:, 1:50);
-%===========Process Test File========================
+
+%==Generate test features, in same order as training features===
 fid2=fopen(test_file);
 tline2 = fgetl(fid2);
 num_col2 = length(find(tline2==',')) + 1;
@@ -40,7 +43,10 @@ test_ids = c2{1};
 %train_features = train_features(:, 1:100);
 %svm_train_features = train_features(:, 1500:2000);
 %svm_train_labels = train_labels(:);
+
+% Cross validation accuracy. Use as test before submission.
 svm_classifier_cv = svmtrain(train_labels, train_features, ['-h 0 -q -v 5 -c 10 -t 2 -g ' num2str(4^(-3))]);  % used 4 for c; RBF kernel function C = 4, gamma = 4^-3
+
 %poly_cv_accu = svmtrain(train_labels, train_features, '-h 0 -q -c 0.25 -v 5 -t 1 -d 1');  % Polynomial
 %svm_accu = svmpredict(test_label, test_data, svm_classifier);
 %svm_accuracy = svm_accu(1);
@@ -49,34 +55,34 @@ svm_classifier_cv = svmtrain(train_labels, train_features, ['-h 0 -q -v 5 -c 10 
 %ens_classifier = fitensemble(train_data, train_label, 'AdaBoostM1', 100,
 %'Tree'); % LogitBoost, Subspace, Bag ; 'AllPredictorCombinations' ; 'KNN'
 %'Discriminant' , 'type', 'classification'
-ens_classifier = fitensemble(train_features, train_labels, 'Bag', 200, 'Tree', 'type', 'classification');
-ens_cv = crossval(ens_classifier, 'KFold', 5);
-ens_error_rate = kfoldLoss(ens_cv);
+%ens_classifier = fitensemble(train_features, train_labels, 'Bag', 200, 'Tree', 'type', 'classification');
+%ens_cv = crossval(ens_classifier, 'KFold', 5);
+%ens_error_rate = kfoldLoss(ens_cv);
 % ens_predictions = predict(ens_classifier, test_data);
 % ens_accuracy = 1 - (nnz(ens_predictions - test_label)/length(test_label));
 % results(t, 4) = ens_accuracy;
 % =================================KNN=====================================
-knn_classifier = fitcknn(train_features, train_labels);
-knn_cv = crossval(knn_classifier, 'KFold', 5);
-knn_error_rate = kfoldLoss(knn_cv);
+% knn_classifier = fitcknn(train_features, train_labels);
+% knn_cv = crossval(knn_classifier, 'KFold', 5);
+% knn_error_rate = kfoldLoss(knn_cv);
 %=============================Discriminant Analysis========================
-disc_anal_classifier = fitcdiscr(train_features, train_labels); 
-disc_anal_cv = crossval(disc_anal_classifier, 'KFold', 5);
-disc_anal_error_rate = kfoldLoss(disc_anal_cv);
+% disc_anal_classifier = fitcdiscr(train_features, train_labels); 
+% disc_anal_cv = crossval(disc_anal_classifier, 'KFold', 5);
+% disc_anal_error_rate = kfoldLoss(disc_anal_cv);
 %============================Logistic Regression===========================
-lr_train_features = train_features(1:19999, 1:100);
-lr_train_labels = train_labels(1:19999);
-log_reg_classifier = mnrfit(lr_train_features, lr_train_labels);
+% lr_train_features = train_features(1:19999, 1:100);
+% lr_train_labels = train_labels(1:19999);
+% log_reg_classifier = mnrfit(lr_train_features, lr_train_labels);
 %log_reg_predictions = mnrval(log_reg_classifier, test_data);
-log_reg_cv = crossval(log_reg_classifier, 'KFold', 5);
-log_reg_error_rate = kfoldLoss(log_reg_cv);
+% log_reg_cv = crossval(log_reg_classifier, 'KFold', 5);
+% log_reg_error_rate = kfoldLoss(log_reg_cv);
 % [~,index_max_prob] = max(log_reg_predictions, [], 2);
 % lg_accuracy = 1 - (nnz(index_max_prob - lg_test_label)/length(lg_test_label));
 % results(t, 1) = lg_accuracy;
 %===========================Naive Bayes============================
-nb_classifier = fitcnb(train_features,train_labels);
-nb_cv = crossval(nb_classifier, 'KFold', 5);
-nb_error_rate = kfoldLoss(nb_cv);
+% nb_classifier = fitcnb(train_features,train_labels);
+% nb_cv = crossval(nb_classifier, 'KFold', 5);
+% nb_error_rate = kfoldLoss(nb_cv);
 
 % CVDiscrMdl = fitcdiscr(X,Y,Name,Value)
 % CVEnsMdl = fitensemble(X,Y,Name,Value)
@@ -87,6 +93,9 @@ nb_error_rate = kfoldLoss(nb_cv);
 
 %===========================Predictions===================================
 %disc_anal_predict = predict(disc_anal_classifier, test_features);
+
+% Run following when generating submission file. Modify file name as
+% necessary.
 svm_classifier = svmtrain(train_labels, train_features, ['-h 0 -q -c 10 -t 2 -g ' num2str(4^(-3))]);  % RBF kernel function C = 4, gamma = 4^-3
 dummy = zeros(9944, 1);
 [svm_predictions, accuracy, ~] = svmpredict(dummy, test_features, svm_classifier);

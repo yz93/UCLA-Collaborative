cbfcd0f934861ac89d33c9d95d5c8922f69d91bc
Zhao Yang <yz931129@gmail.com>
2015-11-30 16:51:00 -0800

    Tuned C parameter of SVM slightly; Accuracy 77.5%; further improvement may require feature selection

--- CS 145/main_zhao.m
+++ CS 145/main_zhao.m
@@ -1,12 +1,12 @@
-train_file = '.\train_3000.csv';
-test_file = '.\test_3000.csv';
-systemCommand = 'C:\Python27\python27.exe generate_train_test_data_1.py 3000';
+train_file = '.\train_2000.csv';
+test_file = '.\test_2000.csv';
+systemCommand = 'C:\Python27\python27.exe generate_train_test_data_1.py 2000';
 system(systemCommand);
 
 fid=fopen(train_file);
 tline = fgetl(fid);
 num_col = length(find(tline==',')) + 1;
-format = ['%d %s' repmat('%f', 1, num_col - 24)];
+format = ['%d %s' repmat('%f', 1, num_col - 12)];
 c = textscan(fid, format, 'Delimiter', ',');
 fclose(fid);
 
@@ -26,7 +26,7 @@ end
 fid2=fopen(test_file);
 tline2 = fgetl(fid2);
 num_col2 = length(find(tline2==',')) + 1;
-format2 = ['%d %s' repmat('%f', 1, num_col2 - 24)];
+format2 = ['%d %s' repmat('%f', 1, num_col2 - 12)];
 c2 = textscan(fid2, format2, 'Delimiter', ',');
 fclose(fid2);
 
@@ -34,13 +34,14 @@ test_features = cell2mat(c2(:, 3:end));
 test_ids = c2{1};
 
 %=========================PCA Analysis=====================================
-[COEFF,SCORE] = princomp(train_features);
-train_centered = SCORE*COEFF';
+%[COEFF,SCORE] = princomp(train_features);
+%train_centered = SCORE*COEFF';
 % ==================svm=================
 %train_features = train_features(:, 1:100);
-svm_train_features = train_features(:, 1500:2000);
-svm_train_labels = train_labels(:);
-svm_classifier_cv = svmtrain(train_labels, train_features, ['-h 0 -q -v 5 -c 4 -t 2 -g ' num2str(4^(-3))]);  % RBF kernel function C = 4, gamma = 4^-3
+%svm_train_features = train_features(:, 1500:2000);
+%svm_train_labels = train_labels(:);
+svm_classifier_cv = svmtrain(train_labels, train_features, ['-h 0 -q -v 5 -c 10 -t 2 -g ' num2str(4^(-3))]);  % used 4 for c; RBF kernel function C = 4, gamma = 4^-3
+%poly_cv_accu = svmtrain(train_labels, train_features, '-h 0 -q -c 0.25 -v 5 -t 1 -d 1');  % Polynomial
 %svm_accu = svmpredict(test_label, test_data, svm_classifier);
 %svm_accuracy = svm_accu(1);
 %results(t, 3) = svm_accuracy;
@@ -48,8 +49,8 @@ svm_classifier_cv = svmtrain(train_labels, train_features, ['-h 0 -q -v 5 -c 4 -
 %ens_classifier = fitensemble(train_data, train_label, 'AdaBoostM1', 100,
 %'Tree'); % LogitBoost, Subspace, Bag ; 'AllPredictorCombinations' ; 'KNN'
 %'Discriminant' , 'type', 'classification'
-ens_classifier = fitensemble(train_features, train_labels, 'Subspace', 'AllPredictorCombinations', 'Discriminant', 'type', 'classification');
-ens_cv = crossval(ens_classifier, 'KFold', 3);
+ens_classifier = fitensemble(train_features, train_labels, 'Bag', 200, 'Tree', 'type', 'classification');
+ens_cv = crossval(ens_classifier, 'KFold', 5);
 ens_error_rate = kfoldLoss(ens_cv);
 % ens_predictions = predict(ens_classifier, test_data);
 % ens_accuracy = 1 - (nnz(ens_predictions - test_label)/length(test_label));
@@ -77,8 +78,6 @@ nb_classifier = fitcnb(train_features,train_labels);
 nb_cv = crossval(nb_classifier, 'KFold', 5);
 nb_error_rate = kfoldLoss(nb_cv);
 
-
-
 % CVDiscrMdl = fitcdiscr(X,Y,Name,Value)
 % CVEnsMdl = fitensemble(X,Y,Name,Value)
 % CVKNNMdl = fitcknn(X,Y,Name,Value)
@@ -88,14 +87,14 @@ nb_error_rate = kfoldLoss(nb_cv);
 
 %===========================Predictions===================================
 %disc_anal_predict = predict(disc_anal_classifier, test_features);
-svm_classifier = svmtrain(train_labels, train_features, ['-h 0 -q -c 4 -t 2 -g ' num2str(4^(-3))]);  % RBF kernel function C = 4, gamma = 4^-3
+svm_classifier = svmtrain(train_labels, train_features, ['-h 0 -q -c 10 -t 2 -g ' num2str(4^(-3))]);  % RBF kernel function C = 4, gamma = 4^-3
 dummy = zeros(9944, 1);
 [svm_predictions, accuracy, ~] = svmpredict(dummy, test_features, svm_classifier);
 prediction_labels = cell(length(svm_predictions), 1);
 for i = 1 : length(svm_predictions)
     prediction_labels{i} = id_to_string(svm_predictions(i));
 end
-out_file = fopen('submission6.csv','w');
+out_file = fopen('submission8.csv','w');
 fprintf(out_file,'id,cuisine\n');
 for i = 1 : length(svm_predictions)
     fprintf(out_file,'%d,%s\n', test_ids(i), prediction_labels{i});
